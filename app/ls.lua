cbedcef499e6ad7530ca8d333d5abd9b930f3104
Tulir293 <tulir293@maunium.net>
2016-06-11 16:21:42 +0300

    Many improvements to ls

--- app/ls.lua
+++ app/ls.lua
@@ -15,27 +15,79 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 function Run(alias, args)
+	local files = {}
+	local directories = {}
+	local dir = nil
+	local longestName = 0
+	local w, h = term.getSize()
+	local maxLength = w - 6
+
 	if #args == 0 then
-		io.Cprintln(colors.green, "app")
+		directories["app"] = "N/A"
+		longestName = string.len("app")
 		if fs.exists("/.ios/startup.lua") then
-			io.Cprintln(colors.white, "startup")
+			files["startup"] = fs.getSize("/.ios/startup.lua")
+			longestName = string.len("startup")
 		end
 
-		for _, file in ipairs(fs.list("/.ios/files")) do
-			if fs.isDir(file) then io.Cprintln(colors.green, file)
-			else io.Cprintln(colors.white, file) end
-		end
+		dir = "/.ios/files"
 	elseif #args == 1 then
 		if args[1] == "app" then
-			for _, file in ipairs(fs.list("/.ios/localapps")) do
-				if fs.isDir then io.Cprintln(colors.green, file)
-				else io.Cprintln(colors.white, file) end
-			end
+			dir = "/.ios/localapps"
 		else
-			for _, file in ipairs(fs.list("/.ios/files/" .. args[1])) do
-				if fs.isDir then io.Cprintln(colors.green, file)
-				else io.Cprintln(colors.white, file) end
+			dir = "/.ios/files/" .. args[1]
+		end
+	else return end
+
+	if dir then
+		for _, file in ipairs(fs.list(dir)) do
+			local name = file
+			local len = string.len(file)
+
+			if len > maxLength then
+				name = string.sub(file, 1, maxLength - 2) .. ".."
+			elseif len > longestName then
+				longestName = len
 			end
+
+			if fs.isDir(file) then directories[name] = fs.getSize(dir .. "/" .. file)
+			else files[name] = fs.getSize(dir .. "/" .. file) end
 		end
 	end
+
+	local x, y = term.getCursorPos()
+	term.setTextColor(colors.white)
+	term.write("file")
+	term.setTextColor(colors.cyan)
+	term.write("/")
+	term.setTextColor(colors.green)
+	term.write("dir")
+	term.setTextColor(colors.cyan)
+	term.write(" name")
+	term.setCursorPos(maxLength, y)
+	term.write("Size")
+	io.Newline()
+
+	local function printFile(file, size)
+		x, y = term.getCursorPos()
+		term.write(file)
+		term.setCursorPos(maxLength, y)
+		term.write(size)
+		io.Newline()
+	end
+
+	term.setTextColor(colors.green)
+	for file, size in pairs(directories) do
+		printFile(file, size)
+	end
+
+	term.setTextColor(colors.white)
+	for file, size in pairs(files) do
+		if string.endsWith(file, ".lua") then
+			file = string.sub(file, 1, string.len(file) - 4)
+		end
+		printFile(file, size)
+	end
+
+	term.setTextColor(io.DEFAULT_COLOR)
 end
