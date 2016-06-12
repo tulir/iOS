be4130db9d53871cd96e9757b56b5e782e3a1192
Tulir293 <tulir293@maunium.net>
2016-06-12 21:46:36 +0300

    Added ducktape to remove app exit keys from input

--- sys/main.lua
+++ sys/main.lua
@@ -103,6 +103,8 @@ end
 function Loop()
 	if startup.PreLoop then startup.PreLoop() end
 	while true do
+		os.startTimer(0.05)
+		os.pullEvent()
 		local cmd, args, termd = io.ReadInput("$", true)
 		if cmd == "exit" then break
 		elseif not termd and cmd then HandleCommand(cmd, args) end
